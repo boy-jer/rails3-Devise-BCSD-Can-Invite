573cdf836774a0b872a46dece4965046e8730017
Steve Alex <salex@mac.com>
2011-10-06 07:04:47 -0500

    invitation_accepted_at added to schema for users
    invitable gem change

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20101027200254) do
     t.string   "loginable_token"
     t.string   "invitation_token",     :limit => 60
     t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
